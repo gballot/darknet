9c5723012193a83c0c90d00f081cfb7fa73b5b6b
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2019-09-17 15:44:51 +0800

    debug parser

--- src/parser.c
+++ src/parser.c
@@ -616,21 +616,26 @@ route_layer parse_route(list *options, size_params params, network *net)
     int *layers = calloc(n, sizeof(int));
     int *sizes = calloc(n, sizeof(int));
     for(i = 0; i < n; ++i){
-        const char * delim = " ,";
-        char *tmp_ref = strtok(l, delim);
+        char *tmp_ref = strtok(l, " ,");
         int index = 0;
+        fprintf(stderr, "In route layer, tmp_ref = '%s'", tmp_ref);
         if(!strcmp(itoa(atoi(tmp_ref), 10), tmp_ref)) {
             index = atoi(tmp_ref);
         } else {
             int found = 0;
             for(int i = 0; i < net->n; i++) {
-                if(!strcmp(net->layers[i].ref, tmp_ref)) {
+                if(net->layers[i].ref && !strcmp(net->layers[i].ref, tmp_ref)) {
                     index = i;
                     found = 1;
                     break;
                 }
             }
-            if(!found) error(strcat("label undefined : ", tmp_ref));
+            if(!found) {
+              char *legend = "label undefined : ";
+              char *message = malloc(strlen(legend) + strlen(tmp_ref));
+              sprintf(message, "%s%s", legend, tmp_ref);
+              error(message);
+            }
         }  
         l = strchr(l, ',')+1;
         if(index < 0) index = params.index + index;
@@ -823,13 +828,6 @@ network *parse_network_cfg(char *filename)
         options = s->options;
         layer l = {0};
         LAYER_TYPE lt = string_to_layer_type(s->type);
-        /* parse ref */
-        char * tmp_ref = option_find_str_quiet(options, "ref", itoa(count, 10));
-        l.ref = malloc(strlen(tmp_ref)*sizeof(char *));
-        char *list_ref = malloc(strlen(tmp_ref)*sizeof(char *));
-        strcpy(l.ref, tmp_ref);
-        strcpy(list_ref, tmp_ref);
-        list_insert(labels, (void *)list_ref);
         if(lt == CONVOLUTIONAL){
             l = parse_convolutional(options, params);
         }else if(lt == DECONVOLUTIONAL){
@@ -896,6 +894,15 @@ network *parse_network_cfg(char *filename)
         }else{
             fprintf(stderr, "Type not recognized: %s\n", s->type);
         }
+        /* parse ref */
+        char * tmp_ref = option_find_str_quiet(options, "ref", itoa(count, 10));
+        l.ref = malloc(strlen(tmp_ref)*sizeof(char *));
+        char *list_ref = malloc(strlen(tmp_ref)*sizeof(char *));
+        strcpy(l.ref, tmp_ref);
+        strcpy(list_ref, tmp_ref);
+        list_insert(labels, (void *)list_ref);
+        fprintf(stderr, "ref='%s'", l.ref);
+        /* end parse ref */
         l.clip = net->clip;
         l.truth = option_find_int_quiet(options, "truth", 0);
         l.onlyforward = option_find_int_quiet(options, "onlyforward", 0);
