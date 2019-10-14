553aae8ea5c9a67c644b13c7d6f59c05b5be8aa1
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2019-10-14 15:52:12 +0800

    correct fspt boxes

--- src/yolo_layer.h
+++ src/yolo_layer.h
@@ -17,7 +17,7 @@ int get_yolo_detections(layer l, int w, int h, int netw, int neth, float thresh,
 void correct_yolo_boxes(detection *dets, int n, int w, int h, int netw, int neth, int relative, int letter);
 extern box get_yolo_box(float *x, float *biases, int n, int index, int i, int j, int lw, int lh, int w, int h, int stride);
 extern int get_yolo_detections(layer l, int w, int h, int netw, int neth, float thresh, int *map, int relative, detection *dets);
-
+extern void correct_yolo_boxes(detection *dets, int n, int w, int h, int netw, int neth, int relative);
 #ifdef GPU
 void forward_yolo_layer_gpu(const layer l, network_state state);
 void backward_yolo_layer_gpu(layer l, network_state state);
