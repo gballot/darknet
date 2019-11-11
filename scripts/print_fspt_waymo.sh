d61ca6a46cebab3c6e4b9895a71ffad10e016343
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2019-11-11 11:41:04 +0800

    add print_fspt_waymo.sh script

---
+++ scripts/print_fspt_waymo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH -o gpu-job-print-fspt-waymo.output
+#SBATCH -p PV1003q,NV100q,PV100q,GV1002q
+#SBATCH --gres=gpu:1
+#SBATCH -n 1
+#SBATCH -c 24
+
+module load cuda90/toolkit
+module load cuda90/blas/9.0.176
+
+datacfg='cfg/waymo.data'
+netcfg='cfg/fspt-waymo.cfg'
+#weightfile='weights/fspt-waymo-data-extraction-day.weights'
+#weightfile='weights/yolov3-waymo.weights'
+options=''
+
+#/home/gballot/NTU/FSPT\ Yolo/darknet/darknet -i 1 fspt train ${datacfg} ${netcfg} ${weightfile} ${options} -gpus 0,1
+/home/gballot/NTU/FSPT\ Yolo/darknet/darknet -i 0 fspt stats ${datacfg} ${netcfg} ${weightfile} ${options} -gpus 0
