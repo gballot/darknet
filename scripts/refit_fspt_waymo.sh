ba654e536f5969fffee18d45b20a431778885249
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2019-12-05 19:36:54 +0800

    scripts

--- scripts/refit_fspt_waymo.sh
+++ scripts/refit_fspt_waymo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -o gpu-job-refit-fspt-waymo-small.output
+#SBATCH -o gpu-job-refit-fspt-waymo-full-new.output
 #SBATCH -p PV1003q,NV100q,PV100q,GV1002q
 #SBATCH --gres=gpu:0
 #SBATCH -n 1
@@ -8,9 +8,9 @@
 module load cuda90/toolkit
 module load cuda90/blas/9.0.176
 
-full='-small'
+full='-full'
 datacfg='cfg/waymo'${full}'.data'
-netcfg='cfg/fspt-waymo'$full'.cfg'
+netcfg='cfg/fspt-waymo'$full'-new.cfg'
 weightfile='weights/fspt-waymo'$full'-data-extraction.weights'
 #weightfile='weights/fspt-waymo'$full'-80-percent-day.weights'
 options='-refit -only_fit -print_stats'
