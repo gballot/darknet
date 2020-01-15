371255b0673c4bee42b7a67446bc97817b8d477d
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2020-01-15 18:57:27 +0800

    modif multiple val script

--- scripts/multiple_val.sh
+++ scripts/multiple_val.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -o gpu-job-multiple-validations-holiday-no-auto.output
+#SBATCH -o gpu-job-multiple-validations-no-auto.output
 #SBATCH -p NV100q,PV100q,GV1002q
 #SBATCH --gres=gpu:1
 #SBATCH -n 1
@@ -14,10 +14,9 @@ prog="/home/gballot/NTU/FSPT Yolo/darknet/darknet"
 tmpprog="${tmpdir}/darknet"
 cp "${prog}" "${tmpprog}"
 
-val_dir='results/multiple_val-holiday-no-auto/'
+val_dir='results/multiple_val-no-auto/'
 mkdir -p "${val_dir}"
 
-output_valid_files=${val_dir}'valid_'
 
 #netcfgs='local_cfg/fspt-waymo-full-multi-0.cfg,local_cfg/fspt-waymo-full-multi-1.cfg,local_cfg/fspt-waymo-full-multi-2.cfg,local_cfg/fspt-waymo-full-multi-3.cfg,local_cfg/fspt-waymo-full-multi-4.cfg,local_cfg/fspt-waymo-full-multi-5.cfg'
 
@@ -27,57 +26,61 @@ negconf='cfg/waymo-full-night.data'
 yolo_thresh='0.7'
 fspt_thresh='0.1,0.3,0.5,0.7,0.8,0.9'
 
-# run several time the process
-netcfgs=""
-for i in {0..20}; do
-    if [ -z "${netcfgs}" ]; then
-        netcfgs="local_cfg/auto/conf${i},"
-    else
-        netcfgs="${netcfgs},local_cfg/auto/conf${i},"
-    fi
-done
-save_weights_file=${val_dir}'weigths_'
-options='-pos '${posconf}' -neg '${negconf}' -ordered -print_stats -yolo_thresh '${yolo_thresh}' -fspt_thresh '${fspt_thresh}' -out '${output_valid_files}' -save_weights_file '${save_weights_file}
+run_confs() {
+    netcfgs=""
+    for (( i=${beg}; i<=${end}; ++i )); do
+        if [ -z "${netcfgs}" ]; then
+            netcfgs="local_cfg/auto/conf${i},"
+        else
+            netcfgs="${netcfgs},local_cfg/auto/conf${i},"
+        fi
+    done
+    output_valid_files=${val_dir}'valid_'${beg}'to'${end}'_'
+    save_weights_file=${val_dir}'weigths_'${beg}'to'${end}'_'
+    options='-pos '${posconf}' -neg '${negconf}' -ordered -print_stats -yolo_thresh '${yolo_thresh}' -fspt_thresh '${fspt_thresh}' -out '${output_valid_files}' -save_weights_file '${save_weights_file}
+    "${tmpprog}" -i 0 fspt valid_multiple ${netcfgs} ${weightfile} ${options} -gpus 0
+}
 
-# run several time the process
-netcfgs=""
-for i in {20..40}; do
-    if [ -z "${netcfgs}" ]; then
-        netcfgs="local_cfg/auto/conf${i},"
-    else
-        netcfgs="${netcfgs},local_cfg/auto/conf${i},"
-    fi
-done
-save_weights_file=${val_dir}'weigths_'
-options='-pos '${posconf}' -neg '${negconf}' -ordered -print_stats -yolo_thresh '${yolo_thresh}' -fspt_thresh '${fspt_thresh}' -out '${output_valid_files}' -save_weights_file '${save_weights_file}
+# run several time the process (up to 575)
+beg=0; end=29
+run_confs
+beg=30; end=59
+run_confs
+beg=60; end=89
+run_confs
+beg=90; end=119
+run_confs
+beg=120; end=149
+run_confs
+beg=150; end=179
+run_confs
+beg=180; end=209
+run_confs
+beg=210; end=239
+run_confs
+beg=240; end=269
+run_confs
+beg=270; end=299
+run_confs
+beg=300; end=329
+run_confs
+beg=330; end=359
+run_confs
+beg=360; end=389
+run_confs
+beg=290; end=419
+run_confs
+beg=420; end=449
+run_confs
+beg=450; end=479
+run_confs
+beg=480; end=509
+run_confs
+beg=510; end=539
+run_confs
+beg=540; end=569
+run_confs
+beg=570; end=575
+run_confs
 
-# run several time the process
-netcfgs=""
-for i in {40..60}; do
-    if [ -z "${netcfgs}" ]; then
-        netcfgs="local_cfg/auto/conf${i},"
-    else
-        netcfgs="${netcfgs},local_cfg/auto/conf${i},"
-    fi
-done
-save_weights_file=${val_dir}'weigths_'
-options='-pos '${posconf}' -neg '${negconf}' -ordered -print_stats -yolo_thresh '${yolo_thresh}' -fspt_thresh '${fspt_thresh}' -out '${output_valid_files}' -save_weights_file '${save_weights_file}
-
-# run several time the process
-netcfgs=""
-for i in {60..575}; do
-    if [ -z "${netcfgs}" ]; then
-        netcfgs="local_cfg/auto/conf${i},"
-    else
-        netcfgs="${netcfgs},local_cfg/auto/conf${i},"
-    fi
-done
-save_weights_file=${val_dir}'weigths_'
-options='-pos '${posconf}' -neg '${negconf}' -ordered -print_stats -yolo_thresh '${yolo_thresh}' -fspt_thresh '${fspt_thresh}' -out '${output_valid_files}' -save_weights_file '${save_weights_file}
-
-
-
-
-"${tmpprog}" -i 0 fspt valid_multiple ${netcfgs} ${weightfile} ${options} -gpus 0
-
-rm -rf "${tmpdir}"
+rm -rf "${tmpdir}" || true
